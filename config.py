4883d4cdd093dafb323a2a42ff33d8f9bb33031b
ShubhamVerma-Developer <shubham25122001@gmail.com>
2024-12-26 17:57:18 +0530

    ------------------- commit for the authentication------------------------------------------------------------

--- config.py
+++ config.py
@@ -4,36 +4,13 @@
 
 import os
 
+""" Bot Configuration """
+
 
 class DefaultConfig:
-    """Bot Configuration"""
+    """ Bot Configuration """
 
     PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
-
-    # GPT-4 Configuration
-    GPT4V_NLP_TO_SQL_KEY = os.environ.get("GPT4V_NLP_TO_SQL_KEY", "")
-    GPT4V_NLP_TO_SQL_ENDPOINT = os.environ.get("GPT4V_NLP_TO_SQL_ENDPOINT", "")
-
-    GPT4V_SQL_TO_NLP_KEY = os.environ.get("GPT4V_SQL_TO_NLP_KEY", "")
-    GPT4V_SQL_TO_NLP_ENDPOINT = os.environ.get(
-        "GPT4V_SQL_TO_NLP_ENDPOINT",
-        "",
-    )
-
-    GPT4V_SQL_TO_GRAPH_KEY = os.environ.get("GPT4V_SQL_TO_GRAPH_KEY", "")
-    GPT4V_SQL_TO_GRAPH_ENDPOINT = os.environ.get(
-        "GPT4V_SQL_TO_GRAPH_ENDPOINT",
-        "",
-    )
-    GPT4V_SQL_TO_GRAPH_MODEL_NAME = os.environ.get("GPT4V_SQL_TO_GRAPH_MODEL_NAME", "")
-    GPT4V_SQL_TO_GRAPH_API_VERSION = os.environ.get(
-        "GPT4V_SQL_TO_GRAPH_API_VERSION", ""
-    )
-
-    # SQL Server Configuration
-    SQL_SERVER = os.environ.get("SQL_SERVER", "")
-    SQL_DB = os.environ.get("SQL_DB", "")
-    SQL_USERNAME = os.environ.get("SQL_USERNAME", "")
-    SQL_PWD = os.environ.get("SQL_PWD", "")
+    CONNECTION_NAME = os.environ.get("ConnectionName", "")
